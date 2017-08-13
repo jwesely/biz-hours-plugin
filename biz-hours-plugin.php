41403f06fedb1d79286fb80b6885de0c84e2ca99
Jeff Wesely <jeff@stonefin.com>
2017-08-13 18:53:02 -0500

    Adding GooglePlaces API to the plugin and letting lat/long and google place id fill automatically

--- biz-hours-plugin.php
+++ biz-hours-plugin.php
@@ -3,13 +3,18 @@
 Plugin Name: Business Hours Plugin
 Plugin URI: none
 Description: Use GooglePlaces to display information about your stores
-Version: 0.2 BETA
+Version: 0.3 BETA
 Author: StoneFin Technologies
 Author URI: https://www.stonefin.com
 */
 if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly
 }
+
+
+
+//include admin settings
+require_once ("admin/biz-hours-plugin-admin-settings.php");
 
 //store location functionality
 require_once ("store-locations.php");
@@ -79,4 +84,9 @@ function wpLocationInstall() {
   add_option( 'wp_location_db_version', $wp_location_db_version );
   flush_rewrite_rules();
 }
+
+
+
+
+
 ?>
